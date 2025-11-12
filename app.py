0b00827f8f9e3a0abc2819d17046cff241d4243b
JacoboHernandez777 <23308060610160@cetis61.edu.mx>
2025-11-12 11:05:17 -0500

    si

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+if not pokemon_name
+flask('por favor ingresa el nombre de un pokemon', 'error')
+return redirect(url_for('index'))
+
+
